4a0551bf7a5452272aa25e12a1b2d7733f1a29fb
24BLOOM <32383831+24BLOOM@users.noreply.github.com>
2020-04-04 20:50:43 +0800

    two version of Snake

---
+++ Snake_version1.1/src/model/Direction.java
@@ -0,0 +1,25 @@
+package model;
+
+
+/**
+ * @author yhyf
+ */
+
+public enum Direction {
+	/**
+	 * Left
+	 */
+	L,
+	/**
+	 * Up
+	 */
+	U,
+	/**
+	 * Right
+	 */
+	R,
+	/**
+	 * Down
+	 */
+	D
+}
